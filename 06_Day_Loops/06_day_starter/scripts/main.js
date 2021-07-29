f007b4d063ac61dcbaa12549018767c42f51da6a
Jacksonmills <jackson.mills@steelseries.com>
2021-07-28 21:39:31 -0500

    6

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,23 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// }
+// for (let i = 5; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 5; i++) {
+//   console.log(`${i} * ${i} = ${i * i}`);
+// }
+
+// const newArray = [];
+// for (let i = 0; i < countries.length; i++) {
+//   newArray.push(countries[i].toUpperCase());
+// }
+// console.log(newArray);
+
+let sum = 0;
+for (let i = 0; i < numbers.length / 2; i++) {
+  sum = sum + numbers[i];
+}
+console.log(sum);
